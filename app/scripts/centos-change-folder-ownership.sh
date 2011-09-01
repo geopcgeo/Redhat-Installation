1aa157d6e8f96d84f8bd13cc20f529167b3d3ef6
Geo <geopc@citrusinformatics.com>
2011-09-01 12:26:50 +0530

    modified

--- app/scripts/centos-change-folder-ownership.sh
+++ app/scripts/centos-change-folder-ownership.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #This script is used to change the ownership of a folder recursively
-chown -R apache /var/www/html/edge.devl.medigy.com/
+chown -R apache:apache /var/www/html/edge.devl.medigy.com/
