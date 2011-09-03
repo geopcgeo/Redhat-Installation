cec37ab8abf19916081588f9f95e3c9b5250a78b
Geo <geopc@citrusinformatics.com>
2011-09-03 15:01:33 +0530

    Completed

--- php/manifests/init.pp
+++ php/manifests/init.pp
@@ -1,16 +1,14 @@
 # Class: php
 #
-# Installs Php 
+# Installs Php
 #
 # Usage:
 # include php
 #
-class php  {
+class php::install {
 
-    include php::params
-
-    package { "php":
-        name   => "${php::params::packagename}",
+       package { "php":
+       name   => "${php::params::packagename}",
         ensure => present,
     }
 
@@ -25,11 +23,39 @@ class php  {
         owner   => "${php::params::configfile_owner}",
         group   => "${php::params::configfile_group}",
         require => Package["php"],
-	notify  => Service["apache"],
+                notify  => Service["apache"],
         ensure  => present,
 
     }
-
 }
 
+class php::modules {
+
+                case $operatingsystem
+                {
+                        debian:
+                        {       package { [ "libapache2-mod-php5","php5-cli","php5-mysql","php5-gd" ]:
+                                ensure => installed,
+                                                }
+                        }
+                        ubuntu:
+                        {       package { [ "libapache2-mod-php5","php5-cli","php5-mysql","php5-gd" ]:
+                                ensure => installed,
+                                                }
+                        }
+                        default:
+                        {       package { ["php-mysql","php-mbstring","php-gd"]:
+                                ensure => installed,
+                                                }
+                        }
+                }
+        }
+
+
+
+
+class php {
+                        require php::params
+                        include php::install, php::modules
+                }
 
